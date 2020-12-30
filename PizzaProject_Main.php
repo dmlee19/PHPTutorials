29c72a9504d68c04a01afae88fa593c9cbf13775
dmlee89 <ldmldm1119@gmail.com>
2020-12-30 17:53:27 +0900

    PHP Pizza Project_03 : Create Main file and include header and footer

---
+++ PizzaProject_Main.php
@@ -0,0 +1,12 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<?php include('PizzaProject_Templates/PizzaProject_Header.php') ?>
+
+<?php include('PizzaProject_Templates/PizzaProject_Footer.php') ?>
+
+</html>
