d98b1d10c9fd491513e8c326a99a4e925781fdf2
dmlee89 <ldmldm1119@gmail.com>
2020-12-31 13:44:52 +0900

    PHP Pizza Project_15 : Update Main file_delete DB connection code then include ConnectDB fiile

--- PizzaProject_Main.php
+++ PizzaProject_Main.php
@@ -1,11 +1,5 @@
 <?php 
-    // Connect to database
-    $connection = mysqli_connect('localhost', 'ldmmysql', 'sql123', 'mario_pizza');
-    
-    // Check connection
-    if(!$connection){//if($connection) --> 연결되면 true
-        echo 'Connect error: ' . mysqli_connect_error();
-    }                           // connection에 문제있는 경우 error 출력
+    include('config/ConnectDB.php');
 
     // Write sql for all pizzas
     $sql = 'SELECT id, title, ingredients, email FROM pizzas ORDER BY created_at';
