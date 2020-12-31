a0c7f60b35e1fa545320713f3290c8dfa95e274d
dmlee89 <ldmldm1119@gmail.com>
2020-12-31 13:30:47 +0900

    PHP Pizza Project_13 : Update Header_input href url (Main, AddPizza)

--- PizzaProject_Templates/PizzaProject_Header.php
+++ PizzaProject_Templates/PizzaProject_Header.php
@@ -22,9 +22,9 @@
 <body class="grey lighten-4">
     <nav class="white z-depth-0">
         <div class="container">
-            <a href="#" class="brand-logo brand-text">Mario Pizza</a>
+            <a href="PizzaProject_Main.php" class="brand-logo brand-text">Mario Pizza</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
-                <li><a href="#" class="btn brand z-depth-0">Add pizza</a></li>
+                <li><a href="PizzaProject_AddPizza.php" class="btn brand z-depth-0">Add pizza</a></li>
             </ul>
         </div>
     </nav>
