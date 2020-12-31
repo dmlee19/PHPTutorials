0284caa00325f5040b377d0aa1309141f028b586
dmlee89 <ldmldm1119@gmail.com>
2020-12-31 10:58:31 +0900

    PHP Pizza Project_10 : Update Main file_Connecting to a database and getting data with MySQL and rendering the data to the browser

--- PizzaProject_Main.php
+++ PizzaProject_Main.php
@@ -1,12 +1,55 @@
 <?php 
+    // Connect to database
+    $connection = mysqli_connect('localhost', 'ldmmysql', 'sql123', 'mario_pizza');
+    
+    // Check connection
+    if(!$connection){//if($connection) --> 연결되면 true
+        echo 'Connect error: ' . mysqli_connect_error();
+    }                           // connection에 문제있는 경우 error 출력
+
+    // Write sql for all pizzas
+    $sql = 'SELECT id, title, ingredients, email FROM pizzas ORDER BY created_at';
+
+    // Make query & get result
+    $result = mysqli_query($connection, $sql);
+
+    // Fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC); //associated array로 저장
+    // print_r($pizzas); // $pizzas array 확인
+
+    // Free result from memory
+    mysqli_free_result($result);
+
+    // Close connection 
+    mysqli_close($connection);
 
 ?>
 
 <!DOCTYPE html>
 <html>
+    <!-- Header -->
+    <?php include('PizzaProject_Templates/PizzaProject_Header.php') ?>
 
-<?php include('PizzaProject_Templates/PizzaProject_Header.php') ?>
-
-<?php include('PizzaProject_Templates/PizzaProject_Footer.php') ?>
+    <h4 class="center grey-text">Pizzas!</h4>
+        <div class="container">
+            <div class="row">
+                <?php foreach($pizzas as $pizza){ ?>
+                    <div class="col s6 md3">
+                        <div class="card z-depth-0">
+                            <div class="card-content center">
+                                <h6><?php echo htmlspecialchars($pizza['title']) ?></h6>
+                                <div><?php echo htmlspecialchars($pizza['ingredients']) ?></div>
+                            </div>
+                            <div class="card-action right-align">
+                                <a href="#" class="brand-text">More Info</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+        
+    <!-- Footer -->
+    <?php include('PizzaProject_Templates/PizzaProject_Footer.php') ?>
 
 </html>
