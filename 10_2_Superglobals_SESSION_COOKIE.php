5bf1b1b8b29cd8f1f8fc5c3450b7e19dbd6af9d6
dmlee89 <ldmldm1119@gmail.com>
2021-01-01 23:59:24 +0900

    add Superglobal_Cookie into Session file then rename

--- 10_2_Superglobals_SESSION_COOKIE.php
+++ 10_2_Superglobals_SESSION_COOKIE.php
@@ -1,6 +1,6 @@
 <?php 
     
-    // Session 
+    // Session for name
     // 서버에 저장되어 서로 다른 페이지에서 사용
 
     if(isset($_POST['submit'])){
@@ -9,6 +9,12 @@
         $_SESSION['name'] = $_POST['name'];
 
         // echo $_SESSION['name']; // input data
+
+
+    //Cookie for gender
+
+        setcookie('gender', $_POST['gender'], time() + 86400);
+        // 쿠키 설정, 86400 (24h --> sec)
 
         header('Location: PizzaProject_Main.php');
         //Project 메인 페이지로 이동
@@ -25,10 +31,12 @@
 
         <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
             <input type="text" name="name">
+            <select name="gender">
+                <option value="male">male</option>
+                <option value="female">female</option>
+            </select>
             <input type="submit" name="submit" value="submit">
         </form>
      
     </body>
 </html>
-
-
